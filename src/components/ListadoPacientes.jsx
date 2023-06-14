d2f1752a8edfaae460f698ddd462f99763511481
Napster <rome.1205h@gmail.com>
2023-06-13 23:59:40 -0500

    updtae

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,7 @@
 import Paciente from "./Paciente"
 
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({pacientes}) => {
    return (
       <div className="md:w-1/2 lg:w-3/5 h-screen md:overflow-y-scroll">
          <h2 className="font-black text-center text-3xl">Listado Pacientes</h2>
@@ -9,9 +9,9 @@ const ListadoPacientes = () => {
             Administra tus {''}
             <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
          </p>
-         <Paciente/>
-         <Paciente/>
-         <Paciente/>
+         {pacientes.map( (paciente) => (
+            <h1>{paciente.name}</h1>
+            ))}
          <Paciente/>
       </div>
    )
