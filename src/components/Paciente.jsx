1f620ba6ff0afd8ef5b74f438ac8c3eee2d91668
Napster <rome.1205h@gmail.com>
2023-06-14 23:18:29 -0500

    add button styles

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -27,6 +27,16 @@
                <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
                   <span className="font-normal normal-case">{sintomas}</span>
                </p>
+               <div className="flex justify-between mt-5">
+                  <button
+                     type="button"
+                     className="py-2 px-2 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                  >Editar</button>
+                  <button
+                     type="button"
+                     className="py-2 px-2 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                  >Eliminar</button>
+               </div>
             </>
       </div>
    )
