f561e956d21be4d379ef9c58b54a24c18c589c52
Napster <rome.1205h@gmail.com>
2023-06-14 23:02:46 -0500

    title dinamico

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,17 +4,30 @@ import Paciente from "./Paciente"
 const ListadoPacientes = ({pacientes}) => {
    return (
       <div className="md:w-1/2 lg:w-3/5 h-screen md:overflow-y-scroll">
-         <h2 className="font-black text-center text-3xl">Listado Pacientes</h2>
-         <p className="text-xl mt-5 mb-10 text-center">
-            Administra tus {''}
-            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-         </p>
-         {pacientes.map( (paciente, index) => (
-               <Paciente
-                  paciente={paciente}
-                  key={index}
-               />
-            ))}
+
+         { pacientes && pacientes.length ? (
+            <>
+               <h2 className="font-black text-center text-3xl">Listado Pacientes</h2>
+               <p className="text-xl mt-5 mb-10 text-center">
+                  Administra tus {''}
+                  <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+               </p>
+               {pacientes.map( (paciente, index) => (
+                  <Paciente
+                     paciente={paciente}
+                     key={index}
+                  />
+               ))}
+            </>
+         ) : (
+            <>
+               <h2 className="font-black text-center text-3xl">No hay Pacientes</h2>
+               <p className="text-xl mt-5 mb-10 text-center">
+                  Comienza agregando pacientes {''}
+                  <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+               </p>
+            </>
+         )}
       </div>
    )
 }
